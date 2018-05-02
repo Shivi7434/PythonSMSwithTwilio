8eedd14f37effbeb2426a54619f40818803f75a9
Shivi7434 <shivalikhanuja.net@gmail.com>
2018-05-01 23:05:16 -0500

    daily commit

--- sms/send_sms.py
+++ sms/send_sms.py
@@ -2,13 +2,13 @@ import os
 from twilio.rest import Client                               #Download the twilio-python library from twilio.com
 
 #create an instance message
-account_sid = "AC049fae2e652501bf8fae0d56d1a70a57"
-auth_token = "101675dc42058f00995e14e9c071f5d1"
+account_sid = ""
+auth_token = ""
 
 client = Client(account_sid, auth_token)
 
 client.messages.create(
-    to="9723337434",
-    from_ = "+14696091218",
+    to="",
+    from_ = "",
     body= "Welcome to this crazy place.You are going die!",
 )
